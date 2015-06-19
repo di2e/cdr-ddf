698be7f04fc0b47c46c9ffa0caa1bca46410c15f
Matt Ramey <matt.ramey@cohesiveintegrations.com>
2015-06-19 16:01:52 -0700

    ECDR-124: Updated to only include content value.

--- transformer/cdr-rome-result-xformer/src/main/java/net/di2e/ecdr/search/transform/response/rome/RomeResponseTransformer.java
+++ transformer/cdr-rome-result-xformer/src/main/java/net/di2e/ecdr/search/transform/response/rome/RomeResponseTransformer.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -94,9 +94,9 @@ public class RomeResponseTransformer implements SearchResponseTransformer {
         }
         SyndContent content = entry.getDescription();
         if ( content.getType().equals( "text/xml" ) ) {
-            metacard.setMetadata( content.toString() );
+            metacard.setMetadata( content.getValue() );
         } else {
-            metacard.setMetadata( "<xml-fragment>" + content.toString() + "</xml-fragment>" );
+            metacard.setMetadata( "<xml-fragment>" + content.getValue() + "</xml-fragment>" );
         }
         return metacard;
     }
